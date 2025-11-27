1240161fbc3208fe46e3304a853a4764ee1f296d
vels3113 <senotovel@gmail.com>
2025-11-27 13:08:15 +0400

    new: added CLI support to load a file

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,32 @@
 #include <QApplication>
+#include <QCommandLineParser>
+#include <QFileInfo>
+#include <QLoggingCategory>
 
 #include "MainWindow.hpp"
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    QCoreApplication::setApplicationName(QStringLiteral("opensvs"));
+
+    QCommandLineParser parser;
+    parser.setApplicationDescription(QStringLiteral("OpenSVS netgen JSON viewer"));
+    parser.addHelpOption();
+    parser.addVersionOption();
+    parser.addPositionalArgument(QStringLiteral("file"), QStringLiteral("Optional netgen JSON report to load on startup."));
+    parser.process(app);
+
     MainWindow window;
+
+    const QStringList positional = parser.positionalArguments();
+    if (!positional.isEmpty()) {
+        const QString filePath = QFileInfo(positional.first()).absoluteFilePath();
+        if (!window.loadFile(filePath, /*showError=*/true)) {
+            qWarning() << "Failed to load file:" << filePath;
+        }
+    }
+
     window.show();
     return app.exec();
 }
