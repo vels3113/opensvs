9fa86c840a5b6c28c5e36c8ccb81469c83a07ca0
vels3113 <senotovel@gmail.com>
2026-01-12 19:17:23 +0400

    infr: added clang-tidy diagnostics

---
+++ scripts/clang-tidy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+./configure.sh --preset debug
+cmake --build build/debug -j$(nproc)
+
+FILES=$(find . -type f ! -path "./build/*" -name '*.cpp' -o -name '*.cc' -o -name '*.cxx' ); 
+
+if [[ -z "$FILES" ]]; then
+  echo "No C++ files to analyze.";
+  exit 0;
+fi;
+
+clang-tidy -p build/debug --quiet $FILES > clang-tidy-report.txt
+echo "Clang-Tidy analysis complete. Report saved to clang-tidy-report.txt."
